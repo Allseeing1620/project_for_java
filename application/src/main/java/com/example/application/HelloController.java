f665f81e0c0b6949d0b1bcfa4f17f624ed4c5287
roman <fiji.filin3@gmail/com>
2022-09-21 00:24:26 +0300

    creating a graphical framework

--- application/src/main/java/com/example/application/HelloController.java
+++ application/src/main/java/com/example/application/HelloController.java
@@ -1,14 +1,40 @@
 package com.example.application;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
 
     @FXML
+    private AnchorPane root_paf;
+
+    private Button btnOpenNewWindow;
+
+    @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        welcomeText.setText("Welcome to Application!");
     }
+
+    @FXML
+    private void loadwin(ActionEvent event) throws IOException {
+
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene path = new Scene(fxmlLoader.load(),600, 400);
+        Stage stage = new Stage();
+        stage.setScene(path);
+        stage.show();
+    }
+
 }
